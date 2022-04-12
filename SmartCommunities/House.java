3a8d0d8633fbe18d29e376e9423fba632fa61bd9
Leonor Caldas <utilizador@pop-os.localdomain>
2022-04-13 00:03:43 +0100

    Construtores e gets e sets completos na class supplier e na classe house ficou a faltar o equals e toString

--- SmartCommunities/House.java
+++ SmartCommunities/House.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Write a description of class House here.
@@ -6,5 +10,92 @@
  * @version (a version number or a date)
  */
 public class House {
-
+    private String address;
+    private String ownerName;
+    private Map<String, SmartDevice> devices;
+    private Map<String, List<String>> rooms;
+    
+    public House(){
+        this.address = "";
+        this.ownerName = "";
+        this.devices = new HashMap<>();
+        this.rooms = new HashMap<>();
+    }
+    
+    public House(String address, String ownerName, Map<String, SmartDevice> devices, Map<String, List<String>> rooms){
+        this.address = address;
+        this.ownerName = ownerName;
+        this.devices = devices;
+        this.rooms = rooms;
+    } 
+    
+    public House(House h){
+        this.address = h.getAddress();
+        this.ownerName = h.getOwnerName();
+        this.devices = h.getDevices();
+        this.rooms = h.getRooms();
+    }
+    
+    public String getAddress(){
+        return this.address;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public String getOwnerName(){
+        return this.address;
+    }
+    
+    public void setOwnerName(String ownerName){
+        this.ownerName = ownerName;
+    }
+    
+    public Map<String, SmartDevice> getDevices(){
+       return this.devices;
+       //return this.devices.values().stream().collect(Collectors.toMap(id.SmartDevice :: getId, id.SmartDevice::clone));
+    }
+    
+    public void setDevices(Map<String, SmartDevice> devices){
+        this.devices = devices;
+    }
+    
+    public Map<String, List<String>> getRooms(){
+       return this.rooms;
+    }
+    
+    public void setRooms(Map<String, List<String>> rooms){
+        this.rooms = rooms;
+    }
+    
+    /*
+    public boolean equals(Object o) {
+        if (this == o) return true; 
+        if ((o == null) || (this.getClass() != o.getClass())) return false; 
+        House h = (House) o; 
+        return price == s.price && 
+               dailyCost == s.dailyCost; 
+    }
+    */
+   
+    public House clone() {
+        return new House(this);    
+    }
+    
+    /*
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nHouse:")
+            .append("\nAddress: ")
+            .append(this.getAddress())
+            .append("\nOwner's Name: ")
+            .append(this.getOwnerName());
+        for(SmartDevice sd : devices){
+            
+        }
+            
+        return sb.toString();
+    }
+    */
 }
--- SmartCommunities/Supplier.java
+++ SmartCommunities/Supplier.java
@@ -6,5 +6,61 @@
  * @version (a version number or a date)
  */
 public class Supplier {
+    private float price;
+    private int dailyCost;
+    
+    public Supplier() {
+        this.price = 0;
+        this.dailyCost = 0;
+    }
+    
+    public Supplier(float price, int dailyCost){
+        this.price = price;
+        this.dailyCost = dailyCost;
+    }
+    
+    public Supplier(Supplier s){
+        this.price = s.getPrice();
+        this.dailyCost = s.getDailyCost();
+    }
+    
+    public float getPrice() {
+        return this.price;
+    }
 
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    public int getDailyCost() {
+        return this.dailyCost;
+    }
+
+    public void setDailyCost(int dailyCost) {
+        this.dailyCost = dailyCost;
+    }
+    
+    public boolean equals(Object o) {
+        if (this == o) return true; 
+        if ((o == null) || (this.getClass() != o.getClass())) return false; 
+        Supplier s = (Supplier) o; 
+        return price == s.price && 
+               dailyCost == s.dailyCost; 
+    }
+
+    public Supplier clone() {
+        return new Supplier(this);    
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nSupplier:")
+            .append("\nPrice: ")
+            .append(this.getPrice())
+            .append("\nDaily Cost: ")
+            .append(this.getDailyCost());
+            
+        return sb.toString();
+    }
 }
+
